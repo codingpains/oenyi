7269534691065986497046c12b660a66d3691e6b
Gus Ortiz <guspidey@gmail.com>
2015-08-29 19:49:55 -0500

    Add resize by contain

--- test.js
+++ test.js
@@ -5,5 +5,12 @@ oenyim('/Users/voxfeed_1/Desktop/gusortiz.jpeg')
   .resize({width : 1000, height: 400, method: 'cover'})
   .exec(function(err, buff) {
     console.log('Err, res', err, buff);
-    fs.writeFile('/Users/voxfeed_1/Desktop/gusortiz-resized.jpg', buff);
+    fs.writeFile('/Users/voxfeed_1/Desktop/gusortiz-cover.jpg', buff);
+  });
+
+oenyim('/Users/voxfeed_1/Desktop/gusortiz.jpeg')
+  .resize({width : 200, height: 1250, method: 'contain'})
+  .exec(function(err, buff) {
+    console.log('Err, res', err, buff);
+    fs.writeFile('/Users/voxfeed_1/Desktop/gusortiz-contain.jpg', buff);
   });
