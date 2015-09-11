35109376126ab98f4c1fbe19954b3cf03a872c4c
Gus Ortiz <guspidey@gmail.com>
2015-09-11 11:44:16 -0500

    fix a lot of style problems

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 var Oenyi = require('./lib/oenyi');
 var BadArgumentError = require('./lib/errors').BadArgumentError;
 
-module.exports = function(pathOrBuffer) {
-  if (!pathOrBuffer || typeof pathOrBuffer === 'string' || pathOrBuffer instanceof Buffer) {
-    return new Oenyi(pathOrBuffer);
+var isPath = function(path) {
+  'use strict';
+  return typeof path === 'string';
+};
+
+var isBuffer = function(buffer) {
+  'use strict';
+  return buffer instanceof Buffer;
+};
+
+module.exports = function(input) {
+  'use strict';
+  if (!input || isPath(input) || isBuffer(input)) {
+    return new Oenyi(input);
   } else {
     throw new BadArgumentError('argument must be a string, a buffer or empty');
   }
